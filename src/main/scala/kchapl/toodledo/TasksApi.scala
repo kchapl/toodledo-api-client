65a3258a44c0b1b8934f95acf0180b4c12668cf6
Kelvin Chappell <kelvin.chappell@guardian.co.uk>
2013-06-18 09:38:35 +0100

    Add filter fields

--- src/main/scala/kchapl/toodledo/TasksApi.scala
+++ src/main/scala/kchapl/toodledo/TasksApi.scala
@@ -19,9 +19,12 @@ object TasksApi {
             modifiedAfter: Option[DateTime] = None,
             completed: Option[Boolean] = None)
            (fetchFields: List[String] = Nil): List[Task] = {
-    //val params = Map("comp" -> "0", "fields" -> "context")
-    val baseParams: Map[String, String] = Map("key" -> key)
-    val params = modifiedBefore.foldLeft(baseParams)((ps, p) => ps + ("modbefore" -> p.toString))
+
+    def addParameter(params: Map[String, String], name: String, value: Option[Any]) = {
+      value.foldLeft(params)((ps, p) => ps + (name -> p.toString))
+    }
+
+    val params = addParameter(addParameter(Map("key" -> key), "modbefore", modifiedBefore), "modafter", modifiedAfter)
     val responseBody = httpClient.makeGetRequest(List("tasks", "get.php"), params)
 
     for {
